9ce7b9e0d2c4a69b807a95de082779e49de5fbad
unknown <bashlykovnikita@gmail.com>
2023-07-06 14:39:15 +0300

    Thired commit

--- sources/source.js
+++ sources/source.js
@@ -6,41 +6,36 @@ const counter = document.getElementById("counter")
 const qRadio = Array.from(document.getElementsByClassName("q-radio"))
 const marker = Array.from(document.getElementsByClassName("marker"))
 const activeMarker = Array.from(document.getElementsByClassName("activeMarker"))
+const greenButton = Array.from(document.getElementsByClassName("answer-button"))[0]
+const redButton = Array.from(document.getElementsByClassName("quit-button"))[0]
 
 const arrayOfActiveTasks = ['1']
 const arrayOfActiveMarkers = []
 // Changing task
+
 taskSpan.forEach(task => {
     task.addEventListener('click', () => {
-        taskListener(task)
-        activeTask[0].classList.remove("activeTask")
-        task.classList.add("activeTask")
-        activeTask[0] = task
-        counter.innerHTML = task.id
-        taskImg.src = changeTaskImg(task)
-
-        // console.log(activeTask[0])
-
+        changingTask(task)
     });
 })
 
 // Changing task answer
 marker.forEach(answer => {
+
     answer.addEventListener('click', () => {
-        activeMarker[0] ? activeMarker[0].classList.remove("activeMarker") : console.log("First One selected")
+        marker.forEach(answer => {
+            if (answer.classList.contains("activeMarker")) {
+                answer.classList.remove("activeMarker")
+            }
+        })
         answer.classList.add("activeMarker")
         activeMarker[0] = answer
-        console.log(answer.id)
-        markListener(answer)
+        markListener(answer, activeTask[0])
+        // buttons(arrayOfActiveTasks, arrayOfActiveMarkers)
+        greenButton.innerHTML = "Відповісти"
+        redButton.innerHTML = "Завершити тест"
     })
 })
-// qRadio.forEach(answer => {
-//     answer.addEventListener('click', () => {
-//         console.log(answer)
-//         markListener(answer)
-//     })
-// })
-
 
 function changeTaskImg(task) {
     for (let i = 0; i < ZNO2019.length; i++) {
@@ -50,12 +45,54 @@ function changeTaskImg(task) {
     }
 }
 
-function taskListener(currentTask) {
-    arrayOfActiveTasks.push(currentTask.id)
-    console.log(arrayOfActiveTasks)
+function taskListener(currentTask, mark) {
+    marker.forEach(answer => {
+        if (answer.classList.contains("activeMarker")) {
+            answer.classList.remove("activeMarker")
+        }
+    })
+    if (arrayOfActiveTasks.indexOf(currentTask.id) >= 0) {
+        arrayOfActiveTasks[arrayOfActiveTasks.indexOf(currentTask.id)] = currentTask.id
+        if (arrayOfActiveMarkers[currentTask.id - 1]) {
+            arrayOfActiveMarkers[currentTask.id - 1].classList.add("activeMarker")
+
+        }
+    } else {
+        console.log(arrayOfActiveTasks.indexOf(currentTask))
+        arrayOfActiveTasks.push(currentTask.id)
+        console.log(arrayOfActiveTasks)
+        if (mark) {
+            mark.classList.remove("activeMarker")
+        }
+    }
 }
 
-function markListener(currentMarker) {
-    arrayOfActiveMarkers.push(currentMarker.id)
-    console.log(arrayOfActiveMarkers)
+function markListener(currentMarker, activeTask) {
+    arrayOfActiveMarkers[activeTask.id - 1] = currentMarker
+
 }
+
+function buttons(taskId, marker) {
+    if (marker[taskId - 1]) {
+        greenButton.innerHTML = "Відповісти"
+        redButton.innerHTML = "Завершити тест"
+    } else {
+        greenButton.innerHTML = "Пропустити"
+        redButton.innerHTML = "Наступне"
+    }
+}
+function changingTask(someTask) {
+    buttons(someTask.id, arrayOfActiveMarkers)
+    taskListener(someTask, activeMarker[0])
+    activeTask[0].classList.remove("activeTask")
+    someTask.classList.add("activeTask")
+    activeTask[0] = someTask
+    counter.innerHTML = someTask.id
+    taskImg.src = changeTaskImg(someTask, activeMarker[0])
+}
+function nextButton(button) {
+    button.addEventListener('click', () => {
+        changingTask(taskSpan[activeTask[0].id])
+    })
+}
+nextButton(greenButton)
