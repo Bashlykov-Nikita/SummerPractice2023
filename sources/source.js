b70b6bf92a75691e4ef1178fc2cc93d3dbc4a95a
unknown <bashlykovnikita@gmail.com>
2023-07-10 17:01:24 +0300

    Sixth commit

--- sources/source.js
+++ sources/source.js
@@ -11,15 +11,27 @@ const redButton = Array.from(document.getElementsByClassName("quit-button"))[0]
 const rows = Array.from(document.getElementsByClassName("r"))
 const fictionRows = Array.from(document.getElementsByClassName("fictionRow"))
 const firstRow = document.getElementById('firstRow')
+const taskAnswerMarkers = Array.from(document.getElementsByClassName("task-answer-markers"))[0]
+const taskAnswerText = Array.from(document.getElementsByClassName("task-answer-text"))[0]
+const inputText = Array.from(document.getElementsByClassName("inptext"))
+const firstTextInputfield = Array.from(document.getElementsByClassName("firstTextInputfield"))[0]
+const secendTextInputfield = Array.from(document.getElementsByClassName("secendTextInputfield"))[0]
+const displayNone27_30 = Array.from(document.getElementsByClassName("displayNone27-30"))
+//
 const arrOfAnswers21_24 = []
+const arrOfAnswers25_26 = []
+
+let checkArrayfor1_20 = []
 
 const taskTypeSwitch = (taskId) => {
     if (taskId < 21) {
         return (0)
     } else if (taskId < 25) {
         return (1)
-    } else {
+    } else if (taskId < 27) {
         return (2)
+    } else if (taskId < 31) {
+        return (3)
     }
 }
 const arrayOfActiveTasks = ['1']
@@ -35,19 +47,19 @@ function sliceIntoRows(arr, rowSize = 5) {
 }
 
 const markersByRows = sliceIntoRows(marker)
-console.log(markersByRows)
 // Changing task
 function changingTask(someTask) {
-    buttons(someTask.id, arrayOfActiveMarkers)
     taskListener(someTask, activeMarker[0])
     activeTask[0].classList.remove("activeTask")
     someTask.classList.add("activeTask")
     arrOfAnswers21_24.length = 0
+    arrOfAnswers25_26.length = 0
     activeTask[0] = someTask
     counter.innerHTML = someTask.id
     taskImg.src = changeTaskImg(someTask, activeMarker[0])
     showAnswerRows(activeTask[0].id)
-
+    // inputs25_26(arrayOfActiveMarkers, activeTask[0].id)
+    buttons(someTask.id, arrayOfActiveMarkers)
 }
 function changeTaskImg(task) {
     for (let i = 0; i < ZNO2019.length; i++) {
@@ -61,8 +73,10 @@ taskSpan.forEach(task => {
         changingTask(task)
     });
 })
+
+
 function taskListener(currentTask, mark) {
-    //Romoving all previous marks
+    //Removing all previous marks
     markersByRows.forEach(row =>
         row.forEach(answer => {
             if (answer.classList.contains("activeMarker")) {
@@ -73,6 +87,9 @@ function taskListener(currentTask, mark) {
     // If task was already seen
     if (arrayOfActiveTasks.indexOf(currentTask.id) >= 0) {
         arrayOfActiveTasks[arrayOfActiveTasks.indexOf(currentTask.id)] = currentTask.id
+        console.log(currentTask.id - 1)
+        buttons(currentTask.id, arrayOfActiveMarkers)
+
         // Put marks down if task was answered previously
         if (arrayOfActiveMarkers[currentTask.id - 1]) {
             switch (taskTypeSwitch(currentTask.id)) {
@@ -86,20 +103,56 @@ function taskListener(currentTask, mark) {
                     console.log(arrayOfActiveMarkers[currentTask.id - 1])
                     arrayOfActiveMarkers[currentTask.id - 1].forEach(row => {
                         if (row) {
+                            console.log(row)
                             row.classList.add("activeMarker")
                         }
                     })
                     break
                 case 2:
-                    console.log('not made yet')
+                    console.log(currentTask.id - 1)
+                    console.log(arrayOfActiveMarkers)
+                    console.log(arrayOfActiveMarkers[currentTask.id - 1][0])
+                    firstTextInputfield.value = arrayOfActiveMarkers[currentTask.id - 1][0]
+                    secendTextInputfield.value = arrayOfActiveMarkers[currentTask.id - 1][1]
+                    break
+                case 3:
+                    console.log(currentTask.id - 1)
+                    console.log(arrayOfActiveMarkers)
+                    console.log(arrayOfActiveMarkers[currentTask.id - 1][0])
+                    firstTextInputfield.value = arrayOfActiveMarkers[currentTask.id - 1][0]
+                    secendTextInputfield.value = arrayOfActiveMarkers[currentTask.id - 1][1]
                     break
             }
         }
         // if it's a new task 
     } else {
+
         arrayOfActiveTasks.push(currentTask.id)
+        if (currentTask.id > 24) { 
+            console.log(`Current task id is ${currentTask.id }`)
+            inputText.forEach(field => {
+                field.addEventListener('input', () => {       
+                    inputs25_26(arrayOfActiveMarkers, currentTask.id)
+                })
+            })
+            inputText.forEach(field => {
+                if (field.value) {
+                    field.value = ''
+                }
+                if (field.placeholder) {
+                    field.value = ''
+                }
+            })
+        }
     }
 }
+markersByRows.forEach(row => {
+    row.forEach(answer => {
+        answer.addEventListener('click', () => {
+            changingMark(row, answer)
+        })
+    })
+})
 
 function changingMark(marksArray, mark) {
 
@@ -111,9 +164,8 @@ function changingMark(marksArray, mark) {
     mark.classList.add("activeMarker")
     activeMarker[0] = mark
     markListener(mark, activeTask[0])
-    //TODO: button function here
-    greenButton.innerHTML = "Відповісти"
-    redButton.innerHTML = "Завершити тест"
+
+
     switch (taskTypeSwitch(activeTask[0].id)) {
         case 0:
             console.log(arrayOfActiveMarkers)
@@ -123,28 +175,19 @@ function changingMark(marksArray, mark) {
             console.log(arrayOfActiveMarkers)
             break
         case 2:
-            console.log('not made yet')
+            console.log(arrayOfActiveMarkers[activeTask[0].id - 1])
+            console.log(arrayOfActiveMarkers)
             break
     }
 }
 
-
-markersByRows.forEach(row => {
-    row.forEach(answer => {
-        answer.addEventListener('click', () => {
-            changingMark(row, answer)
-        })
-    })
-})
-
-
-
 function markListener(currentMarker, activeTask) {
-    //TODO: Change the way how markers works in tasks 21-25
     switch (taskTypeSwitch(activeTask.id)) {
         case 0:
             arrayOfActiveMarkers[activeTask.id - 1] = currentMarker
             taskSpan[activeTask.id - 1].classList.add("alreadyAnswered")
+            buttons(activeTask.id, arrayOfActiveMarkers)
+            //console.log(true)
             break
         case 1:
             if (currentMarker.classList.contains('row1')) {
@@ -156,36 +199,50 @@ function markListener(currentMarker, activeTask) {
             } else if (currentMarker.classList.contains('row4')) {
                 arrOfAnswers21_24[3] = currentMarker
             }
-            let buf = [...arrOfAnswers21_24]
-            arrayOfActiveMarkers[activeTask.id - 1] = buf
+            let buf21_24 = [...arrOfAnswers21_24]
+            arrayOfActiveMarkers[activeTask.id - 1] = buf21_24
+            buttons(activeTask.id, arrayOfActiveMarkers)
             taskSpan[activeTask.id - 1].classList.add("alreadyAnswered")
             break
         case 2:
-            console.log('not made yet')
+            console.log(true)
+            // arrayOfActiveMarkers[activeTask.id - 1] = inputs25_26()
+            // taskSpan[activeTask.id - 1].classList.add("alreadyAnswered")
             break
     }
 }
 
-//Changes buttons inner text if answered is marked
-function buttons(taskId, marker) {
-    if (marker[taskId - 1]) {
-        greenButton.innerHTML = "Відповісти"
-        redButton.innerHTML = "Завершити тест"
-    } else {
-        greenButton.innerHTML = "Пропустити"
-        redButton.innerHTML = "Наступне"
-    }
+
+function inputs25_26(arrOfAnswers, currentTaskId) {
+    
+
+    console.log(arrOfAnswers25_26)
+    console.log(currentTaskId)
+    // console.log(true)
+
+    arrOfAnswers25_26[0] = firstTextInputfield.value
+    arrOfAnswers25_26[1] = secendTextInputfield.value
+    console.log(arrOfAnswers25_26)
+    let buf25_26 = [...arrOfAnswers25_26]
+    arrOfAnswers[currentTaskId - 1] = buf25_26
+    taskSpan[currentTaskId - 1].classList.add("alreadyAnswered")
+
 }
 
-function nextButton(button) {
-    button.addEventListener('click', () => {
-        changingTask(taskSpan[activeTask[0].id])
-    })
-}
-nextButton(greenButton)
+
+
+
+
+
+
+
+
+
 function showAnswerRows(currentTaskId) {
     switch (taskTypeSwitch(currentTaskId)) {
         case 0:
+            taskAnswerText.classList.add("displayNone")
+            taskAnswerMarkers.classList.remove("displayNone")
             for (let i = 1; i < rows.length; i++) {
                 rows[i].classList.add("displayNone")
             }
@@ -194,6 +251,8 @@ function showAnswerRows(currentTaskId) {
             })
             break
         case 1:
+            taskAnswerText.classList.add("displayNone")
+            taskAnswerMarkers.classList.remove("displayNone")
             rows.forEach(row => {
                 row.classList.remove("displayNone")
             })
@@ -202,7 +261,81 @@ function showAnswerRows(currentTaskId) {
             })
             break
         case 2:
-            console.log('not made yet')
+            displayNone27_30.forEach(el => el.classList.remove("displayNone"))
+            taskAnswerMarkers.classList.add("displayNone")
+            taskAnswerText.classList.remove("displayNone")
+            // inputs25_26(arrayOfActiveMarkers, currentTaskId)
             break
+        case 3:
+            taskAnswerMarkers.classList.add("displayNone")
+            taskAnswerText.classList.remove("displayNone")
+            displayNone27_30.forEach(el => el.classList.add("displayNone"))
+
+        // inputs25_26(arrayOfActiveMarkers, currentTaskId)
     }
-}  
+}
+
+//Changes buttons inner text if answered is marked
+function nextButton() {
+    changingTask(taskSpan[activeTask[0].id])
+    console.log(activeTask[0].id)
+}
+
+
+greenButton.addEventListener('click', () => {
+    nextButton()
+})
+//nextButton(greenButton)
+// nextButton(redButton)
+
+function buttons(taskId, marker) {
+    if (marker[taskId -1]){ 
+        console.log(marker[taskId])
+        greenButton.innerHTML = "Відповісти"
+        redButton.innerHTML = "Завершити тест"
+    } else {
+        // console.log(taskId)
+        greenButton.innerHTML = "Пропустити"
+        redButton.innerHTML = "Наступне"
+    }
+}
+function endButton(button) {
+    button.addEventListener('click', () => {
+        if (button.innerHTML == "Завершити тест") {
+            console.log("end")
+            end()
+        } else {
+            nextButton()
+        }
+    })
+}
+
+buttons(activeTask[0].id - 1, arrayOfActiveMarkers)
+endButton(redButton)
+
+function end() {
+    for (let i = 0; i < 20; i++) {
+        if (arrayOfActiveMarkers[i]) {
+            checkArrayfor1_20.push(arrayOfActiveMarkers[i].id)
+            // console.log(arrayOfActiveMarkers[i].id)
+        } else {
+            console.log(undefined)
+        }
+    }
+    for (let i = 20; i < 25; i++) {
+        if (arrayOfActiveMarkers[i]) {
+            for(let j = 0; j < 4; j++){
+                if(arrayOfActiveMarkers[i][j]){
+                    // console.log(arrayOfActiveMarkers[i][j].id)
+                    checkArrayfor1_20.push(arrayOfActiveMarkers[i].id)
+                }else{
+                    console.log(undefined)
+                }
+            }
+            //console.log(arrayOfActiveMarkers[i])
+        } else {
+            console.log(undefined)
+        }
+    }
+    console.log(checkArrayfor1_20)
+}
